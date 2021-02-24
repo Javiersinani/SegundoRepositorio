2d0c93a5c7e9baf4dc3df1386ab8507f82bc3fc1
Javier Siñani Lijeron <a2019113929@estudiantes.upsa.edu.bo>
2021-02-24 19:11:12 -0400

    Emocion 3 con modelo luck

--- Pronostico/Pronosticotiempo/Controllers/EmotionsController.cs
+++ Pronostico/Pronosticotiempo/Controllers/EmotionsController.cs
@@ -1,12 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Pronosticotiempo.Controllers
+﻿namespace Pronosticotiempo.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+    using Pronosticotiempo.Models;
+
     [Route("api/[controller]")]
     [ApiController]
     public class EmotionsController : ControllerBase
@@ -36,6 +32,18 @@ namespace Pronosticotiempo.Controllers
                 return "sos una persona aburrida";
             return "estoy muy ocupado, no te puedo atender en este momento";
 
+        }
+        [HttpPost]
+        [Route("ConModelo")]
+        public string Emocion3([FromBody] Luck luck)
+        {
+            if (luck.LuckId == 0)
+                return "Sos una persona indiferente";
+            if (luck.LuckId == 1)
+                return "sos una persona feliz";
+            if (luck.LuckId == 3)
+                return "sos una persona aburrida";
+            return "estoy muy ocupado, no te puedo atender en este momento";
         }
     }
 }
